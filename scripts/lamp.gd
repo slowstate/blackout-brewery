8ebae266a33425cae20e49fdc2ec39c2acb8775b
IceRain <icewinds26@gmail.com>
2024-07-23 19:53:29 +0200

    Fixed bugs
    Fixed lamp and workshop button

--- scripts/lamp.gd
+++ scripts/lamp.gd
@@ -1,27 +1,19 @@
-extends CharacterBody2D
+extends Node2D
 
-var is_dragging = false
-var offset: Vector2
-var mouse_in = false
+var draggable = false
+var mouse_offset = Vector2(0, 0)
 
 func _process(delta):
-	if mouse_in:
-		if Input.is_action_just_pressed("Left_Mouse_Button") and not is_dragging:
-			offset = get_global_mouse_position() - global_position
-			is_dragging = true
-		
-		if Input.is_action_pressed("Left_Mouse_Button") and is_dragging:
-			global_position = get_global_mouse_position() - offset
-		elif Input.is_action_just_released("Left_Mouse_Button"):
-			is_dragging = false
+	if draggable:
+		followMouse()
 
-func _on_draggable_area_body_entered(body):
-	if body == self:
-		mouse_in = true
-	
+func followMouse():
+	position = get_global_mouse_position() + mouse_offset
 
-
-func _on_draggable_area_body_exited(body):
-	if body == self:
-		mouse_in = false
-		is_dragging = false
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			mouse_offset = position - get_global_mouse_position()
+			draggable = true
+		else:
+			draggable = false

f37860d35bdbaa3e868d5db8f31ecf23b0b9f78d
slowstate <slowstatedev@gmail.com>
2024-07-23 22:33:57 +1000

    Added basic potion brewing logic and customer order timeouts

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,6 +4,7 @@ var customer_scene = preload("res://scenes/customer.tscn")
 
 @onready var shop = $Shop
 @onready var workstation = $Workstation
+@onready var customer_spawn_timer = $CustomerSpawnTimer
 
 var current_customer
 
@@ -11,11 +12,14 @@ var current_customer
 func _ready():
 	shop.visible = true
 	workstation.visible = false
+	
+	# Start spawn timer for first customer
+	customer_spawn_timer.wait_time = randi_range(3, 5)
+	customer_spawn_timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_shop_shop_button_pressed():
 	shop.visible = false
@@ -30,5 +34,12 @@ func _on_workstation_workstation_button_pressed():
 func _on_customer_spawn_timer_timeout():
 	if current_customer != null:
 		current_customer.queue_free()
-	var new_customer = customer_scene.instantiate()
-	shop.add_child(new_customer)
+	current_customer = customer_scene.instantiate()
+	current_customer.customerOrderTimeout.connect(_customer_order_timeout)
+	shop.add_child(current_customer)
+
+
+func _customer_order_timeout():
+	current_customer.queue_free()
+	customer_spawn_timer.wait_time = randi_range(3, 5)
+	customer_spawn_timer.start()

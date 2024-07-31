fb1c71c97176ca24daee422f00c1e3f1bc739168
IceRain <icewinds26@gmail.com>
2024-07-31 12:34:14 +0200

    game door_fx

--- scripts/audio_player.gd
+++ scripts/audio_player.gd
@@ -2,6 +2,7 @@ extends AudioStreamPlayer
 
 const title_music =preload("res://assets/music/BB_titlev1_OwenGrey.wav")
 const shop_music = preload("res://assets/music/BB_gameplayv1_OwenGrey.wav")
+const door_fx = preload("res://assets/music/squeaky.mp3")
 
 func _play_music(music: AudioStream, volume = 0.0):
 	if stream == music:
@@ -13,4 +14,14 @@ func _play_music(music: AudioStream, volume = 0.0):
 	
 func play_music_level(audio):
 	_play_music(audio)
-		
+
+func play_FX(stream: AudioStream, volume = 0.0):
+	var fx_player = AudioStreamPlayer.new()
+	fx_player.stream = stream
+	fx_player.name = "FX_PLAYER"
+	fx_player.volume_db = volume
+	add_child(fx_player)
+	fx_player.play()
+	
+	await fx_player.finished
+	fx_player.queuefree()
